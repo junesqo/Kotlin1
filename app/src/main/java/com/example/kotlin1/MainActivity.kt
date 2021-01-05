4f3905641301c1e75e891b3de3b58acd80f88612
junesqo <junesqo@gmail.com>
2021-01-05 20:46:08 +0600

    on click show toast

--- app/src/main/java/com/example/kotlin1/MainActivity.kt
+++ app/src/main/java/com/example/kotlin1/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.kotlin1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.id_event -> Toast.makeText(this, "Events", Toast.LENGTH_SHORT).show()
+            R.id.id_profile -> Toast.makeText(this, "Your profile", Toast.LENGTH_SHORT).show()
+            R.id.id_about -> Toast.makeText(this, "About us", Toast.LENGTH_SHORT).show()
+        }
+
+        return true
     }
 }
